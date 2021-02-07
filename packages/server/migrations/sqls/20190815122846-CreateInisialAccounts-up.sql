9f69261da9190317bcd30e82f849a02aa9e4ef25
gen4 <gen4mail@gmail.com>
2021-02-07 13:57:21 +0300

    project start up fixes

--- packages/server/migrations/sqls/20190815122846-CreateInisialAccounts-up.sql
+++ packages/server/migrations/sqls/20190815122846-CreateInisialAccounts-up.sql
@@ -1,3 +1,4 @@
 
 INSERT INTO users (email, admin) values ('tim@newflight.co', true);
 INSERT INTO users (email, admin) values ('mateusz@russak.biz', true);
+INSERT INTO users (email, admin) values ('313515@gmail.com', true);
