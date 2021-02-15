34617a9ec3dab3a5d019c7734c3552a7b58dc9c8
gen4 <gen4mail@gmail.com>
2021-02-16 02:04:42 +0300

    csv

---
+++ packages/server/src/services/CsvService.js
@@ -0,0 +1,40 @@
+import stringify from 'csv-stringify';
+import parse from 'csv-parse';
+import _ from 'lodash'
+
+function generateCSV(data){
+    console.log('>> ')
+    stringify(data, {header: true},function(err, output){
+        console.log(output)
+        console.log('<< ')
+        prs(output)
+      })
+      
+}
+
+function prs(input){
+    parse(input, function(err, output){
+        const keys = _.take(output)[0]
+        const e = _.map(output, (i) => {
+            console.log([keys, i])
+            return _.fromPairs(_.unzip([keys, i]))
+        })
+        
+        _.each(e, (i) => {
+            i.required = i.required === '1'
+        })
+        console.log(e)
+      })
+}
+
+const data = [ // example transformations / displayName = frontend field label / chapterMarker = preview timecode
+    { type: "text", layerName: "CLIENT_NAME", property: "Source Text", displayName: "Customer Name", titleField: true, required: true, chapterMarker: "1.5" },
+    { type: "image", layerName: "clientLogo.png", composition: "*", displayName: "Customer Logo", required: true, chapterMarker: "1.5" },
+{ type: "colour", layerName: "BRAND_PRIMARY", property: "Source Text", displayName: "Primary Background", required: true, chapterMarker: "19" },
+    { type: "audio", layerName: "audio_intro.wav", composition: "*", displayName: "Introduction Audio", required: true },
+    { type: "image", layerName: "mobileLogin.jpg", composition: "*", displayName: "Mobile Login", required: true, chapterMarker: "25"}
+]
+
+generateCSV(data)
+
+export default generateCSV
