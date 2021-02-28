1ff0691d9a3c47b09b8b9604169be3728ae903a5
gen4 <gen4mail@gmail.com>
2021-02-28 11:44:52 +0300

    upd

--- packages/worker/src/esm.js
+++ packages/worker/src/esm.js
@@ -2,7 +2,7 @@ const path = require('path')
 const dotenv = require('dotenv')
 const envpath = path.normalize(path.join(__dirname, '../../..', '.env'))
 console.log(' > > > > ENVPATH = ', envpath)
-const dotenvError = dotenv.config()
+const dotenvError = dotenv.config({ path: envpath })
 console.log('DEE ', dotenvError)
 process.env.CONFIG = process.env.CONFIG || 'worker';
 // eslint-disable-next-line no-global-assign
